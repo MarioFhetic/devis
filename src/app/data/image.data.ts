5b1b9a0fc8cdf517a842356bd3e096d5487113df
MarioFhetic <fayollemario@outlook.fr>
2020-05-06 16:38:54 +0200

    new build relative path

--- src/app/data/image.data.ts
+++ src/app/data/image.data.ts
@@ -1,35 +1,35 @@
 export const images = [
     {
       id: 1,
-      url: "../../assets/img1.jpg"
+      url: "/assets/img1.jpg"
     },
     {
       id: 2,
-      url: "../../assets/img2.jpg"
+      url: "/assets/img2.jpg"
     },
     {
       id: 3,
-      url: "../../assets/img3.jpg"
+      url: "/assets/img3.jpg"
     },
     {
       id: 4,
-      url: "../../assets/img4.jpg"
+      url: "/assets/img4.jpg"
     },
     {
       id: 5,
-      url: "../../assets/img5.jpg",
+      url: "/assets/img5.jpg",
       caption: true,
     },
     {
       id: 6,
-      url: "../../assets/img6.jpg",
+      url: "/assets/img6.jpg",
     },
     {
       id: 7,
-      url: "../../assets/img3.jpg",
+      url: "/assets/img3.jpg",
     },
     {
       id: 8,
-      url: "../../assets/img8.jpg",
+      url: "/assets/img8.jpg",
     },
   ];
