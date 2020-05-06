825d0b8feedc5d342f80f577724ab6b6d7380465
MarioFhetic <fayollemario@outlook.fr>
2020-05-06 16:48:53 +0200

    new build relative path

--- src/app/data/image.data.ts
+++ src/app/data/image.data.ts
@@ -1,35 +1,35 @@
 export const images = [
     {
       id: 1,
-      url: "/assets/img1.jpg"
+      url: "https://mariofhetic.github.io/assets/img1.jpg"
     },
     {
       id: 2,
-      url: "/assets/img2.jpg"
+      url: "https://mariofhetic.github.io/assets/img2.jpg"
     },
     {
       id: 3,
-      url: "/assets/img3.jpg"
+      url: "https://mariofhetic.github.io/assets/img3.jpg"
     },
     {
       id: 4,
-      url: "/assets/img4.jpg"
+      url: "https://mariofhetic.github.io/assets/img4.jpg"
     },
     {
       id: 5,
-      url: "/assets/img5.jpg",
+      url: "https://mariofhetic.github.io/assets/img5.jpg",
       caption: true,
     },
     {
       id: 6,
-      url: "/assets/img6.jpg",
+      url: "https://mariofhetic.github.io/assets/img6.jpg",
     },
     {
       id: 7,
-      url: "/assets/img3.jpg",
+      url: "https://mariofhetic.github.io/assets/img3.jpg",
     },
     {
       id: 8,
-      url: "/assets/img8.jpg",
+      url: "https://mariofhetic.github.io/assets/img8.jpg",
     },
   ];
